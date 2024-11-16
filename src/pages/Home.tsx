060c26736cb1746eb6eb077d2cc46f9cbd2594cc
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-11-17 03:52:30 +0800

    Added React Three Drei Dependecy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
 // <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POPUP</div>
 const Home = () => {
   return (
   <section className='w-full h-screen'>
+    <Canvas 
+              className='w-full h-screen bg-transparent'
+              camera={{ near: 0.1, far: 1000 }} children={undefined}    >
+    </Canvas>
+    <Suspense fallback={<Loader />}>
+        
+    </Suspense>
+
 
   </section>
   )
