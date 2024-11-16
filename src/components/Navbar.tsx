1665451db683254832b12422a7a605dcd4cf294c
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-11-17 03:42:47 +0800

    Added React Three Fiber dependency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,10 @@ const Navbar = () => {
         <NavLink to="/" className="bg-white flex justify-center items-center shadow-lg w-10 h-10 rounded-lg uppercase">
         <p className='blue-gradient_text'>JRO</p>
         </NavLink>
-
-
         <nav className="flex justify-center items-center space-x-4">
         <NavLink to="/about" className={({ isActive }) => `${isActive ? 'text-blue-500 font-bold' : 'text-black'}`}>About</NavLink>
-        <NavLink to="/contact" className={({ isActive }) => `${isActive ? 'text-blue-500 font-bold' : 'text-black'}`}>Contact</NavLink></nav>
+        <NavLink to="/contact" className={({ isActive }) => `${isActive ? 'text-blue-500 font-bold' : 'text-black'}`}>Contact</NavLink>
+        </nav>
     </header>
   )
 }
