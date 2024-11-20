96f20b036e5e2d68f1a52245e98ba2f5accd2a0b
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-11-21 01:46:26 +0800

    Added smooth effect after pressing arrow keys

--- src/models/Island.tsx
+++ src/models/Island.tsx
@@ -64,9 +64,11 @@ const Island = ( props: any ) => {
     if(e.key === "ArrowLeft") {
       if(!isRotating) setIsRotating(true);
       islandRef.current.rotation.y += 0.01 * Math.PI;
+      rotationSpeed.current = 0.0125;
     }else if (e.key === "ArrowRight") {
       if(!isRotating) setIsRotating(true);
       islandRef.current.rotation.y -= 0.01 * Math.PI;
+      rotationSpeed.current = -0.0125;
     }
   }
 
@@ -1823,5 +1825,7 @@ const Island = ( props: any ) => {
     </a.group>
   )
 }
+
+useGLTF.preload(islandScene);
 
 export default Island
