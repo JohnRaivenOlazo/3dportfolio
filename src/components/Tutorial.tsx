00ccbfee32b101c38979020108d8d0ee7cf14e0f
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-11-21 00:23:57 +0800

    Adjust tutorial position for better user experience

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -2,7 +2,7 @@ import drag from '../assets/drag.png'
 
 const Tutorial = () => {
     return (
-      <div className="absolute top-20 left-0 right-0 flex justify-center items-center flex-col cursor-grab">
+      <div className="absolute top-28 sm:top-24 left-0 right-0 flex justify-center items-center flex-col cursor-grab">
         <img src={drag} className="w-6 h-6 block animate-slide-loop" />
         <p className="font-medium text-xs sm:text-lg text-center animate-pulse 1s ease-in-out">
           Drag to Explore
