6dc1324682041fb25148707ca2cbf7dac21e3f66
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-11-24 22:24:27 +0800

    Adjust title size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <header className="header flex justify-between items-center p-4 fixed top-0 w-full z-50 px-5">
       <NavLink to="/" className="flex flex-row justify-center items-center gap-0 backdrop-blur-sm rounded-lg p-1">
-        <img src={logo} alt="logo" className="w-6 h-6" />
-        <p className='blue-gradient_text font-bold text-md italic uppercase'>ohn Raiven Olazo</p>
+        <img src={logo} alt="logo" className="w-5 h-5 sm:w-6 sm:h-6" />
+        <p className='blue-gradient_text font-bold sm:text-md text-xs  italic uppercase'>ohn Raiven Olazo</p>
       </NavLink>
       <nav className="relative flex justify-center items-center space-x-4 bg-transparent px-4 py-2 font-bold text-md md:text-xl backdrop-blur-sm rounded-lg p-2">
         <p className="relative group">
