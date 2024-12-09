c29a4196701abf3491e43d7ef15b85c878073cc3
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-12-10 04:45:50 +0800

    Fix footer text color

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <div className="mt-36 footer-container flex flex-col items-center justify-center p-0 sm:p-4">
-      <p className="text-sm">
-        © 2024 <span className="font-semibold text-gray-400">John Raiven Olazo</span>. All rights reserved.
+      <p className="text-sm text-gray-400">
+        © 2024 <span className="font-semibold text-gray-200">John Raiven Olazo</span>. All rights reserved.
       </p>
     </div>
   );
