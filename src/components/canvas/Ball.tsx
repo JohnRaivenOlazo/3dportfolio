99e120b00981591784bc689c43b09a4b81255c33
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-12-11 01:47:47 +0800

    test

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -7,22 +7,28 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
+
 import Loader from "../Loader";
 
 const Ball = (props : any) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh scale={2.75}>
-        <sphereGeometry args={[1, 64, 64]} />
-        <meshStandardMaterial color="#fff" />
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={0.5}
+          scale={1}
           map={decal}
         />
       </mesh>
@@ -32,11 +38,16 @@ const Ball = (props : any) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas>
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<Loader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
