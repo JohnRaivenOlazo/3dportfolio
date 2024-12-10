f50a449438b434a64044aa3e2725352fefe26eb2
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-12-11 02:45:35 +0800

    Debug

--- src/components/sections/Technologies.tsx
+++ src/components/sections/Technologies.tsx
@@ -1,9 +1,9 @@
-import { useInView } from "react-intersection-observer";
+
 import { technologies } from "../../assets/data";
 import BallCanvas from "../canvas/Ball";
 
 const Technologies = () => {
-  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.0 });
+
 
   return (
     <div id="technologies" className="text-white">
@@ -12,10 +12,10 @@ const Technologies = () => {
         <p className="text-gray-400 mb-12 text-sm">
           Here's a <span className="text-gray-100">selection</span> of tools and technologies I know and love working with.
         </p>
-        <div ref={ref} className="grid grid-cols-3 lg:grid-cols-5 justify-center lg:justify-around gap-4">
+        <div className="grid grid-cols-3 lg:grid-cols-5 justify-center lg:justify-around gap-4">
           {technologies.map((tech, index) => (
             <div key={`${tech.name}-${index}`} className="flex flex-col items-center w-16 h-16">
-              {inView && <BallCanvas icon={tech.icon} />}
+              <BallCanvas icon={tech.icon} />
             </div>
           ))}
         </div>
