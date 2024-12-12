b0bd818c049a446a4e2ec19cd4130a04ffe503dc
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-12-12 21:24:15 +0800

    Add responsiveness to welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -21,13 +21,13 @@ const WelcomePage = ({ setIsWelcomePage }: { setIsWelcomePage: Function }) => {
   animate={isExiting ? { opacity: 0 }: { opacity: 1,scale: 1 }}
   transition={{ duration: 0.8, ease:[0.22, 1, 0.36, 1] }}
 >
-      <button className={`relative top-0 w-1/4 bg-gray-400 h-14 rounded-full text text-white text-center font-bold 
+      <button className={`relative top-0 w-60 bg-gray-400 h-12 sm:h-14 rounded-full text text-white text-center font-bold 
       ${loading ? "cursor-not-allowed opacity-50" : "cursor-pointer"}`}
         disabled={loading}
         onClick={handleExit}
       >
-        <div className="absolute top-0 bg-teal-500 h-14 rounded-full transition-all duration-300" style={{ width: `${progress}%` }} />
-        <p className="absolute inset-0 flex items-center justify-center text-2xl">{loading ? "Loading..." : <>Explore World <FaArrowRight className="ml-2 inline-block"/></>}</p>
+        <div className="absolute top-0 bg-teal-500 h-12 sm:h-14 rounded-full transition-all duration-300" style={{ width: `${progress}%` }} />
+        <p className="absolute inset-0 flex items-center justify-center text-xl sm:text-2xl">{loading ? "Loading..." : <>Explore World <FaArrowRight className="ml-1 sm:ml-2 inline-block"/></>}</p>
       </button>
     </motion.div>
   );
