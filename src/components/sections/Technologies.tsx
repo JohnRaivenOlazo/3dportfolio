d3383c7fb41ce87371ce532e396c1629c014dfe6
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2024-12-18 18:38:12 +0800

    Improve text in technology section

--- src/components/sections/Technologies.tsx
+++ src/components/sections/Technologies.tsx
@@ -9,7 +9,7 @@ const Technologies = () => {
       <div className="text-white">
         <h2 className="text-2xl sm:3xl font-bold mb-3">My Tech Stack.</h2>
         <p className="text-gray-400 mb-10 text-sm">
-          I actively use this wide range of tools and technologies, and I continue to <span className="text-gray-200">explore</span> new ones. I love working with these technologies and am always <span className="text-gray-200">eager</span> to learn.
+          I actively use this wide range of tools, and I continue to <span className="text-gray-200">explore</span> new ones. I love working with these technologies and am always <span className="text-gray-200">eager</span> to learn.
         </p>
         <div ref={ref} className="grid grid-cols-3 lg:grid-cols-6 justify-items-center gap-4 mx-auto py-4">
           {technologies.slice(0,6).map((tech, index) => (
